afd79c77930efbd00f5193a3b430d8ebe49c1381
marianapt <mariana_pt3@hotmail.com>
2017-11-06 15:26:17 -0600

    added a read me

---
+++ README.md
@@ -0,0 +1,8 @@
+Find Friend app finds the best possible match 
+
+uses the information stored in data to compare the new test score.
+
+
+
+I used node for this project. 
+
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express')
 var app = express ()
+var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 8080;
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false})
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.use(bodyParser.json ({ type: 'application/**json'}))
+app.use(bodyParser.json({ type: 'application/**json'}))
 
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type'}))
 
